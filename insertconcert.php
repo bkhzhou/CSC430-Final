8d24a2275f4da6f94e583934c84d372150598860
bkhzhou <79166987+bkhzhou@users.noreply.github.com>
2022-05-17 11:40:36 -0400

    Update insertconcert.php
    fix date format insert into DB

--- insertconcert.php
+++ insertconcert.php
@@ -3,17 +3,21 @@
 	if (session_status() === PHP_SESSION_NONE) {
 		session_start();
 	}
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
 	
-	
-	$concertdate = $_REQUEST['concert_date'];
+	$concertdate = date('Y-m-d', strtotime($_REQUEST['concert_date']));
 	$concertname = $_REQUEST['concert_name'];
 	$concertloc = $_REQUEST['concert_loc'];
+
+	echo $concertdate . " " . $concertname . " " . $concertloc;
 
 	if(isset($_POST['submit'])){
 		$stmt = "INSERT INTO `concert`(`concert_date`, `concert_name`, `concert_loc`) VALUES ('$concertdate', '$concertname', '$concertloc')";
 		mysqli_query($conn, $stmt);
 	}
 
-	header("Location: adminportal.php");
+	header("Location: adminportal.html");
 	mysqli_close($conn);
 ?>
